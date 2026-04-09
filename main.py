a2031cabe929f79f0ddd98b4c771da11a5d684c4
neuralnexlabs22 <rohithlakshman999@gmail.com>
2026-04-09 16:05:42 +0530

    updated version

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-import pandas as pd
+import pandas as pd 
 from src.preprocess import clean_text
-from src.sentiment import analyze_sentiment
 from src.keywords import extract_keywords
+from src.sentiment import analyze_text
 
-df = pd.read_csv ("data/review.csv")
+df = pd.read_csv("data/review.csv")
 
-df = df.dropna(subset=['review'])
+df = df.head(10000)
+
+df = df.dropna(subset=[['review']])
 
 df['cleaned'] = df['review'].apply(clean_text)
 
-df['sentiment'] = df['cleaned'].apply(analyze_sentiment)
+df['sentiment'] = df['cleaned'].apply(lambda x: analyze_text(x)['label'])
+df['sentiment'] = df['cleaned'].apply(lambda x: analyze_text(x)['score'])
 
-df['keyboard'] = df['cleaned'].apply(lambda x: extract_keywords(x))
-
+df['keywords'] = df['cleaned'].apply(lambda x: extract_keywords(x))
 
 df.to_csv("data/output.csv", index=False)
 
-print("Analyzes Completed !")
+print("Analysis Completed ✅")
