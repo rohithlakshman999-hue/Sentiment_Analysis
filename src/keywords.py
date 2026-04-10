b080038edc5e37459646d9846c19f4e7922c9da2
neuralnexlabs22 <rohithlakshman999@gmail.com>
2026-04-10 21:55:45 +0530

    changes has been made 1

--- src/keywords.py
+++ src/keywords.py
@@ -1,20 +1,16 @@
-import streamlit as st 
 from keybert import KeyBERT
 
-
-@st.cache_resource
-def load_model():
-    return KeyBERT()
-
-kw_model = load_model()
+kw_model = KeyBERT()
 
 def extract_keywords(text):
-    if not text or len(text.strip())==0:
-        return[]
-    
-    keywords=kw_model.extract_keywords(
+    if not text:
+        return []
+
+    keywords = kw_model.extract_keywords(
         text,
-        keyphrase_ngram_range=(1,2),
-        stop_words="english",
+        keyphrase_ngram_range=(1, 2),
+        stop_words='english',
         top_n=5
     )
+
+    return [kw[0] for kw in keywords]
